5ec98bf0defb9550ae61b65822b59e1490be235e
ZenNgesheya <225042738@STUDENTS.UNAM.NA>
2026-06-06 02:47:01 +0200

    feat: add location verification logic with error handling

--- screens/VerifyLocationScreen.js
+++ screens/VerifyLocationScreen.js
@@ -6,3 +6,28 @@ FIREBASE_MESSAGING_SENDER_ID=345242762024
 FIREBASE_APP_ID=1:345242762024:web:9284f25927732c444d6e1d
 CLOUDINARY_CLOUD_NAME=dlbjuvumj
 CLOUDINARY_UPLOAD_PRESET=ongwediva_reports
+useEffect(() => {
+    verifyLocation();
+  }, []);
+
+  const verifyLocation = async () => {
+    setVerifying(true);
+    setError(null);
+    
+    try {
+      const isValid = await isWithinOngwediva();
+      if (isValid) {
+        setVerified(true);
+        setVerifying(false);
+        setTimeout(() => {
+          navigation.replace('Home');
+        }, 2000);
+      } else {
+        setVerified(false);
+        setVerifying(false);
+      }
+    } catch (err) {
+      setError(err.message);
+      setVerifying(false);
+    }
+  };
